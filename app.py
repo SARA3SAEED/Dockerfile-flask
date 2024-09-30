5a0d4201c5485010f24cd7d1b8a065200b3b6fb4
faialotaibi <f.alotaibi@tuwaiq.edu.sa>
2024-10-01 01:26:33 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask App!"
+
+@app.route('/about')
+def about():
+    return "This is a simple Flask application."
+
+@app.route('/contact')
+def contact():
+    return "Feel free to contact us!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
